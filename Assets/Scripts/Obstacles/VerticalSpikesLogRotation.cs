50805cf4a92e0550f8896c81a37fd32077454f3e
Orest Shchukin <orest.shchukin@gmail.com>
2025-08-22 04:58:34 -0700

    Added rotating X prefabs

--- Assets/Scripts/Obstacles/VerticalSpikesLogRotation.cs
+++ Assets/Scripts/Obstacles/VerticalSpikesLogRotation.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class VerticalSpikesLogRotation : MonoBehaviour
 {
     [SerializeField] float rotationSpeed = 360f; // degrees per second
+    [SerializeField] bool inverted = false;
+
+    private int modifier = 1;
+    void Awake()
+    {
+        if (inverted) modifier = -1;
+    }
     void Update()
     {
-        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+        transform.Rotate(0f, modifier * rotationSpeed * Time.deltaTime, 0f);
     }
 
 }
