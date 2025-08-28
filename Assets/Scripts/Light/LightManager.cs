68a7ca513e2ef1e0125149589e3359daa22cbc95
Orest Shchukin <orest.shchukin@gmail.com>
2025-08-28 07:25:00 -0700

    Added LightManagerScript to the Directional Light

---
+++ Assets/Scripts/Light/LightManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+
+    
+    [SerializeField] private Vector3 MorningDLPos = new Vector3(320f, 196f, 326f);
+    [SerializeField] private Vector3 AfternoonDLPos = new Vector3(320f, 196f, 326f);
+    [SerializeField] private Vector3 NightDLPos = new Vector3(320f, 196f, 326f);
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
